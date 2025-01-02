69526f79db0c5d60060bfffb9df351c24de3f373
jia-niang <1@paperplane.cc>
2025-01-02 16:56:30 +0800

    chore(web): 新增 MODE 到 import.meta.env 中

--- packages/web/src/vite-env.d.ts
+++ packages/web/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly MODE: 'development' | 'production'
   readonly VITE_BASE_URL: string
   readonly VITE_WEBSITE_TITLE: string
   readonly VITE_API_BASE_URL: string
