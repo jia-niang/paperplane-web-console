85a224c2aa2faf1f4c6209fba0c87003664f130e
jia-niang <1@paperplane.cc>
2025-01-04 19:00:16 +0800

    chore(ci): 现在输出 .deps 时，还会打印目录结构；更新 commitlint 配置

--- ci/prepare-docker.js
+++ ci/prepare-docker.js
@@ -3,29 +3,43 @@ const path = require('path')
 
 const outputDir = '.deps'
 const subModuleDir = 'packages'
-const copyFiles = ['package.json', 'pnpm-lock.yaml', 'pnpm-workspace.yaml', '.npmrc']
+const copyFiles = ['package.json', '.npmrc', 'pnpm-lock.yaml', 'pnpm-workspace.yaml']
 
 function extraDependencyDefinition() {
   fs.rmSync(outputDir, { recursive: true, force: true })
   fs.mkdirSync(outputDir, { recursive: true })
-
-  for (const file of copyFiles) {
-    if (fs.existsSync(file)) {
-      fs.copyFileSync(file, path.join(outputDir, file))
-    }
-  }
+  console.log(`${outputDir}`)
 
   const subModuleNames = fs.readdirSync(subModuleDir)
   for (const subModuleName of subModuleNames) {
     const subModulePath = path.join(subModuleDir, subModuleName)
     const subModuleOutputDir = path.join(outputDir, subModulePath)
-    for (const file of copyFiles) {
+
+    console.log(` ├── ${subModuleName}`)
+
+    const fileList = copyFiles.filter(f => fs.existsSync(path.resolve(subModulePath, f)))
+    for (const file of fileList) {
+      fs.mkdirSync(subModuleOutputDir, { recursive: true })
       if (fs.existsSync(path.join(subModulePath, file))) {
-        fs.mkdirSync(subModuleOutputDir, { recursive: true })
         fs.copyFileSync(path.join(subModulePath, file), path.join(subModuleOutputDir, file))
+
+        const isLastFile = fileList.indexOf(file) >= fileList.length - 1
+        console.log(isLastFile ? ` │   └── ${file}` : ` │   ├── ${file}`)
       }
     }
   }
+
+  const fileList = copyFiles.filter(f => fs.existsSync(f))
+  for (const file of fileList) {
+    if (fs.existsSync(file)) {
+      fs.copyFileSync(file, path.join(outputDir, file))
+
+      const isLast = fileList.indexOf(file) >= fileList.length - 1
+      console.log(isLast ? ` └── ${file}` : ` ├── ${file}`)
+    }
+  }
+
+  console.log(`\nDone.`)
 }
 
 function prepareDocker() {
