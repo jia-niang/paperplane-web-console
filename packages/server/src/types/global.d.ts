eca331d43f1f4a74f4a79bba8d336a767fcd117c
jia-niang <1@paperplane.cc>
2025-01-05 03:45:05 +0000

    refactor(server/def): 删除无用注释

--- packages/server/src/types/global.d.ts
+++ packages/server/src/types/global.d.ts
@@ -4,18 +4,3 @@ declare namespace NodeJS {
     readonly NODE_ENV: 'development' | 'production'
   }
 }
-
-// import { OnModuleInit } from '@nestjs/common'
-// import { Prisma, PrismaClient } from '@repo/db'
-// import { PrismaServiceOptions } from 'nestjs-prisma'
-
-// declare module 'nestjs-prisma' {
-//   declare class PrismaService
-//     extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'info' | 'warn' | 'error'>
-//     implements OnModuleInit
-//   {
-//     private readonly prismaServiceOptions
-//     constructor(prismaServiceOptions?: PrismaServiceOptions)
-//     onModuleInit(): Promise<void>
-//   }
-// }
